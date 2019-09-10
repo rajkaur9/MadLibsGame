e6106e3f73dbeac6b8caf3107f42fab91d99db83
rajkaur9 <rkaur12@mail.csuchico.edu>
2019-09-10 12:34:54 -0700

    A funny story

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,31 @@
-//Authors: 
 #include <iostream>
 #include <string>
 
-int main()
-{
+using namespace std;
 
-  return 0;
+main()
+
+{
+ int a; 
+        string adjective;
+        cout << "enter adjective";
+        cin >> adjective;
+
+        string noun;
+        cout << "enter noun";
+        cin >> noun;
+
+        string verb;
+        cout << "enter verb ";
+        cin >> verb;
+
+        string number;
+        cout << "enter number";
+        cin >> number;
+
+        cout << "Once upon a time a"; cout << adjective; cout << noun; cout << "was"; 
+        cout << verb; cout << "through a forest with an AX to find food, but walking home would be a";
+        cout << number; cout << "mile walk";
+
+        return 0;
 }
